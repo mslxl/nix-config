b3ff958e1b2a254d18bc26fec5787adbc05ed1d4
mslxl <i@mslxl.com>
2021-02-13 19:08:32 +0800

    [zsh] Organize zsh config

--- dotfiles/zsh/var.zsh
+++ dotfiles/zsh/var.zsh
@@ -1,20 +1,17 @@
 #!/bin/zsh
 
-export EDITOR="vim"
+# Misc
+export EDITOR="emacsclient -a \"\" -c -nw"
 export BROWSER="firefox"
+export PATH=$HOME/.local/bin/:$PATH
+export PATH=$HOME/bin:$PATH
 
-# export PUB_HOSTED_URL=https://pub.flutter-io.cn
-# export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
+# Flutter
 export FLUTTER_STORAGE_BASE_URL="https://mirrors.tuna.tsinghua.edu.cn/flutter"
 export PUB_HOSTED_URL="https://mirrors.tuna.tsinghua.edu.cn/dart-pub"
 
-export RANGER_LOAD_DEFAULT_RC=FALSE
-
-# export PATH=$HOME/.stack/programs/x86_64-linux/ghc-tinfo6-8.8.2/bin:$PATH
-export PATH=$HOME/.local/bin/:$PATH
-export PATH=$HOME/bin:$PATH
-export PATH=$HOME/.zsh/Script/:$PATH
-# export PATH=$HOME/Android/flutter/bin/:$HOME/Android/Sdk/platform-tools/:$PATH
 # Doom Emacs
 export PATH=$HOME/.emacs.d/bin/:$PATH
 
+# Ranger
+export RANGER_LOAD_DEFAULT_RC=FALSE
