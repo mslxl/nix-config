e4e0bf1609f8ef651efc3ae8c039fa173379972d
mslxl <i@mslxl.com>
2021-08-31 12:10:29 +0800

    feat(dotfile): impl encryption feature by python

--- dotfiles/zsh/var.zsh
+++ dotfiles/zsh/var.zsh
@@ -28,3 +28,5 @@ export PATH=$(yarn global bin):$PATH
 
 # Android SDK
 export ANDROID_SDK_ROOT=$HOME/Android/Sdk
+
+#secret[senstive data]{jA0EBwMCp82/9aItjxfu0sAQAeRhFswIbL0PrYcfSgV6TE0jqrGyoES56CgxwTnsq3rXvIuFvlSMoh2Nvrh8wmgpC2t8Tsk+k7yZHglEHF4lTSflBmaEzxOkmbVXqpWnHKmL472JUxRSagK6aHbEgqddROTJ+jc31CaCbUX/XXKDsR9PG1o7ifgwkRUt18PIegE7EAEovNpu5r63XUyyTFDOsvzaQfY5Q8UWSxI8LGIe82PJ4XbQQIwU482M5XaoIMwaSQIcB3TFHoOSk7BXSkS+830WHIJg6/jOhIYsLjRgWg==}
