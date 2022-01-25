42ddef1f17762fc1d1215de9916dea56d1065fd9
mslxl <i@mslxl.com>
2022-01-25 09:38:24 +0800

    add zsh

---
+++ .zsh/var.zsh
@@ -0,0 +1,36 @@
+#!/bin/zsh
+
+# Misc
+hash emacs 2>/dev/null && {
+    export EDITOR="emacs -nw"
+}
+export BROWSER="firefox"
+export PATH=$HOME/.local/bin/:$PATH
+export PATH=$HOME/script:$PATH
+export PATH=$HOME/.local/share/coursier/bin:$PATH
+
+# Flutter
+export FLUTTER_STORAGE_BASE_URL="https://mirrors.tuna.tsinghua.edu.cn/flutter"
+export PUB_HOSTED_URL="https://mirrors.tuna.tsinghua.edu.cn/dart-pub"
+
+# Go
+export GOPATH=~/.go
+export GOPROXY=https://goproxy.cn
+
+# Java (with arch-linux)
+export JAVA_HOME=/usr/lib/jvm/default/
+
+# Doom Emacs
+export PATH=$HOME/.emacs.d/bin/:$PATH
+
+# Ranger
+export RANGER_LOAD_DEFAULT_RC=FALSE
+
+# Nodejs
+hash yarn 2>/dev/null && {
+    export PATH=$(yarn global bin):$PATH
+}
+
+# Hide GStreamer warnings
+export OPENCV_LOG_LEVEL=ERROR
+
