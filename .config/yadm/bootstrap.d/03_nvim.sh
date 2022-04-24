be347f7a486149a099f5ab4644017b875a3bedb9
mslxl <i@mslxl.com>
2022-04-24 21:28:12 +0800

    add lunarvim config

--- .config/yadm/bootstrap.d/03_nvim.sh
+++ .config/yadm/bootstrap.d/03_nvim.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-if [[ -d "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim" ]]; then
-    echo "Nvim packer.nvim already exists, skipped"
-else
-    git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
-fi
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
