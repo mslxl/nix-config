dd89960b8f345658278125d6fa7b2367ff32581f
mslxl <i@mslxl.com>
2023-11-13 13:04:07 +0800

    为 minecraft 添加多个 jre

--- home/prog/minecraft.nix
+++ home/prog/minecraft.nix
@@ -3,7 +3,6 @@
 {
   # Maybe use falke and direnv is a better choice?
   home.packages = with pkgs; [
-    jre
-    prismlauncher
+    (prismlauncher.override { jdks = [ jdk8 jdk17 jdk adoptopenjdk-jre-openj9-bin-11 ]; })
   ];
 }
