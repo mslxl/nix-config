0ace3edba4eb643eb1bc656ea9d42321609ecf59
mslxl <i@mslxl.com>
2024-03-07 00:41:01 +0800

    hyprland: polish screen scale on xiaoxin

--- systems/profiles.nix
+++ systems/profiles.nix
@@ -16,8 +16,18 @@ in {
     background = wallpaper + /keyvisual_1.jpg;
     hyprland = {
       enable = true;
-      monitors = [",2560x1600,auto,1"];
-      extraConfig = '''';
+      monitors = [
+        "eDP-1,2560x1600,0x0,1.25"
+        "HDMI-A-1,1920x1080,64x1280,1"
+      ];
+      extraConfig = ''
+      xwayland {
+        force_zero_scaling = 1
+      }
+
+      # default is 96, 96 * 1.25 is 120
+      exec = bash -c "echo 'Xft.dpi: 120' | xrdb -merge"
+      '';
     };
     sway = {
       enable = false;
