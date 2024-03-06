cffe7ca3560a91b835b03d221643f9148ccf6f9d
mslxl <i@mslxl.com>
2024-03-07 00:42:20 +0800

    env: mount samba on xiaoxin

--- hosts/xiaoxinpro16-2021/default.nix
+++ hosts/xiaoxinpro16-2021/default.nix
@@ -78,7 +78,17 @@ in {
   environment.systemPackages = with pkgs; [
     lshw
     glxinfo
+    cifs-utils
   ];
+
+  fileSystems."/mnt/samba" = {
+    device = "//192.168.1.128/secret";
+    fsType = "cifs";
+    options = let
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
+    in [ "${automount_opts},credentials=/mnt/samba-secret,uid=1000,gid=100" ];
+    # TODO: setup secret via agenix
+  };
 
   powerManagement.enable = true;
   services.logind = {
