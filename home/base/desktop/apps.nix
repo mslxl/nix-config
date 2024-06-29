0e81b7c77bb338a3a57d79a75572a880cc6bca44
mslxl <i@mslxl.com>
2024-06-29 09:05:26 +0800

    xdg: add typora and obsidian link

--- home/base/desktop/apps.nix
+++ home/base/desktop/apps.nix
@@ -4,6 +4,16 @@
   lib,
   ...
 }: {
+  xdg.desktopEntries.obsidian = {
+    name = "Obsidian";
+    categories = ["Office"];
+    terminal = false;
+    exec = "obsidian --enable-features=UseOzonePlatform --enable-wayland-ime %u";
+    comment = "Knowledge base";
+    icon = "obsidian";
+    mimeType = ["x-scheme-handler/obsidian"];
+    type = "Application";
+  };
   home.packages = with pkgs; [
     obsidian
     telegram-desktop
