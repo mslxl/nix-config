8f166bcd1dd9ac8919760f92ea6723ecdb2dc2bc
mslxl <i@mslxl.com>
2024-07-09 09:50:18 +0800

    git: more convenience alias

--- home/base/server/git.nix
+++ home/base/server/git.nix
@@ -24,6 +24,12 @@
       ck = "checkout";
       br = "branch";
       fomo = "!git fetch origin main && git rebase origin/main";
+      # 删除最近的提交，保留文件修改
+      undo = "reset --soft HEAD^";
+      # 删除最近一个提交，不保留文件
+      cancel = "reset --hard HEAD^";
+      # 提交完了，发现还需要一点小修改，不想新建一个提交
+      onemore = "commit -a --amend --no-edit";
     };
 
     ignores = [
