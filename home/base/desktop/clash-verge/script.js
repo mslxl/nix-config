d40d3672583958d63b14723a44935e77dd93e8ef
mslxl <i@mslxl.com>
2025-01-10 13:55:55 +0800

    clash-verge-rev: add global url-test proxy group in script

--- home/base/desktop/clash-verge/script.js
+++ home/base/desktop/clash-verge/script.js
@@ -1,31 +1,43 @@
 // Define main function (script entry)
 
 function main(config, profileName) {
+  
   // Github API Rate Limits
   config['proxy-groups'].push({
     name:'GitHub API',
     type: 'url-test',
-    url: 'https://api.github.com/orgs/spring',
-    "expected-status": "200",
-    interval: 86400,
+    url: 'https://api.github.com/',
+    "expected-status": 200,
+    interval: 10*1000,
     proxies: config['proxies']
       .map((proxy)=>proxy.name)
   })
   config['rules'].splice(1, 0, "DOMAIN,api.github.com,GitHub API")
 
- 
-  // Japanese Blacklist Website
+  // Auto Proxy
   config['proxy-groups'].push({
-    name:'No JAP Proxies',
+    name:'ext.AutoProxies',
     type: 'url-test',
     url: 'http://www.gstatic.com/generate_204',
     interval: 86400,
     proxies: config['proxies']
-      .filter((proxy)=>!/(^JP)|(日本)/.test(proxy.name))
       .filter((proxy)=>!/[(自动选择)(故障转移)(剩余流量)(套餐到期)]/.test(proxy.name))
       .map((proxy)=>proxy.name)
   })
-  config['rules'].splice(1, 0, "DOMAIN,hxcy.top,No JAP Proxies")
+  config['proxy-groups'][0].proxies.splice(0, 0, "ext.AutoProxies")
+
+  // Japanese Blacklist Website
+  config['proxy-groups'].push({
+    name:'No Asia Proxies',
+    type: 'url-test',
+    url: 'http://www.gstatic.com/generate_204',
+    interval: 86400,
+    proxies: config['proxies']
+      .filter((proxy)=>!/(^JP)|(日本)|(韩)|(KO?R)/.test(proxy.name))
+      .filter((proxy)=>!/[(自动选择)(故障转移)(剩余流量)(套餐到期)]/.test(proxy.name))
+      .map((proxy)=>proxy.name)
+  })
+  config['rules'].splice(1, 0, "DOMAIN-SUFFIX,hxcy.top,No Asia Proxies")
 
   return config;
 }
