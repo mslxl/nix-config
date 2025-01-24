bd7333afb08e7a6c247fec875f88bf9640d7496c
mslxl <i@mslxl.com>
2025-01-24 22:30:38 +0800

    clash-verge-rev: update script.js

--- home/base/desktop/clash-verge/script.js
+++ home/base/desktop/clash-verge/script.js
@@ -12,7 +12,7 @@ function main(config, profileName) {
 
   // Github API Rate Limits
   config['proxy-groups'].push({
-    name:'GitHub API',
+    name:'userScript.GitHubAPI',
     type: 'url-test',
     url: 'https://api.github.com/',
     "expected-status": 200,
@@ -20,11 +20,11 @@ function main(config, profileName) {
     proxies: config['proxies']
       .map((proxy)=>proxy.name)
   })
-  config['rules'].splice(1, 0, "DOMAIN,api.github.com,GitHub API")
+  config['rules'].splice(1, 0, "DOMAIN,api.github.com,userScript.GitHubAPI")
 
   // Auto Proxy
   config['proxy-groups'].push({
-    name:'ext.AutoProxies',
+    name:'userScript.AutoProxies',
     type: 'url-test',
     url: 'http://www.gstatic.com/generate_204',
     interval: 86400,
@@ -32,11 +32,13 @@ function main(config, profileName) {
       .filter((proxy)=>!/[(自动选择)(故障转移)(剩余流量)(套餐到期)]/.test(proxy.name))
       .map((proxy)=>proxy.name)
   })
-  config['proxy-groups'][0].proxies.splice(0, 0, "ext.AutoProxies")
+  // Set default proxy for first proxy-groups
+  // Usually the first proxy-group is default proxy route
+  config['proxy-groups'][0].proxies.splice(0, 0, "userScript.AutoProxies")
 
   // Japanese Blacklist Website
   config['proxy-groups'].push({
-    name:'No Asia Proxies',
+    name:'userScript.NoAsiaProxies',
     type: 'url-test',
     url: 'http://www.gstatic.com/generate_204',
     interval: 86400,
@@ -45,7 +47,7 @@ function main(config, profileName) {
       .filter((proxy)=>!/[(自动选择)(故障转移)(剩余流量)(套餐到期)]/.test(proxy.name))
       .map((proxy)=>proxy.name)
   })
-  config['rules'].splice(1, 0, "DOMAIN-SUFFIX,hxcy.top,No Asia Proxies")
+  config['rules'].splice(1, 0, "DOMAIN-SUFFIX,hxcy.top,userScript.NoAsiaProxies")
 
   return config;
 }
