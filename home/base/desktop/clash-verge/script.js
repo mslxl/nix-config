60a2bf89bad93e04da45dfa796b3a39ba5f55d28
mslxl <i@mslxl.com>
2025-02-03 02:14:52 +0800

    clash-verge-rev: add deepseek to proxy

--- home/base/desktop/clash-verge/script.js
+++ home/base/desktop/clash-verge/script.js
@@ -2,13 +2,15 @@
 
 function main(config, profileName) {
   // Netease Mumu Player 12 Ad-Blocker
-  [ "DOMAIN,shence-api.mumu.163.com,REJECT"
-  , "DOMAIN,nemu.fp.ps.netease.com,REJECT"
-  , "DOMAIN,mumu.nie.netease.com,REJECT"
-  , "DOMAIN,hubble.netease.com,REJECT"
-  , "DOMAIN,store-api.mumu.163.com,REJECT"
-  ].map(rule=>config['rules'].splice(1, 0, rule))
+  [ "DOMAIN,shence-api.mumu.163.com"
+  , "DOMAIN,nemu.fp.ps.netease.com"
+  , "DOMAIN,mumu.nie.netease.com"
+  , "DOMAIN,hubble.netease.com"
+  , "DOMAIN,store-api.mumu.163.com"
+  ].forEach(rule=>config['rules'].splice(1, 0, `${rule},REJECT`));
 
+  [ "DOMAIN,wakatime.mslxl.com"
+  ].forEach(rule=>config['rules'].splice(1, 0, `${rule},DIRECT`));
 
   // Github API Rate Limits
   config['proxy-groups'].push({
@@ -19,8 +21,8 @@ function main(config, profileName) {
     interval: 10*1000,
     proxies: config['proxies']
       .map((proxy)=>proxy.name)
-  })
-  config['rules'].splice(1, 0, "DOMAIN,api.github.com,userScript.GitHubAPI")
+  });
+  config['rules'].splice(1, 0, "DOMAIN,api.github.com,userScript.GitHubAPI");
 
   // Auto Proxy
   config['proxy-groups'].push({
@@ -31,10 +33,16 @@ function main(config, profileName) {
     proxies: config['proxies']
       .filter((proxy)=>!/[(自动选择)(故障转移)(剩余流量)(套餐到期)]/.test(proxy.name))
       .map((proxy)=>proxy.name)
-  })
+  });
   // Set default proxy for first proxy-groups
   // Usually the first proxy-group is default proxy route
-  config['proxy-groups'][0].proxies.splice(0, 0, "userScript.AutoProxies")
+  config['proxy-groups'][0].proxies.splice(0, 0, "userScript.AutoProxies");
+
+  // Deepseek oversea version
+  [ "DOMAIN-SUFFIX,deepseek.com"
+  , "DOMAIN-SUFFIX,volces.com"
+  , "DOMAIN-SUFFIX,portal101.cn"
+  ].forEach(rule=>config['rules'].splice(1, 0, `${rule},userScript.AutoProxies`));
 
   // Japanese Blacklist Website
   config['proxy-groups'].push({
@@ -46,8 +54,11 @@ function main(config, profileName) {
       .filter((proxy)=>!/(^JP)|(日本)|(韩)|(KO?R)/.test(proxy.name))
       .filter((proxy)=>!/[(自动选择)(故障转移)(剩余流量)(套餐到期)]/.test(proxy.name))
       .map((proxy)=>proxy.name)
-  })
-  config['rules'].splice(1, 0, "DOMAIN-SUFFIX,hxcy.top,userScript.NoAsiaProxies")
+  });
+  
+  [ "DOMAIN,hxcy.top"
+  , "DOMAIN,galgame.best"
+  ].forEach(rule=>config['rules'].splice(1, 0, `${rule},userScript.NoAsiaProxies`));
 
   return config;
 }
