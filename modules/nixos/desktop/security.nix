6250bd534a987211e98c3bfcd403929d2ea8b2ef
mslxl <i@mslxl.com>
2025-08-30 18:05:03 +0800

    ssh: fix conflict agent

--- modules/nixos/desktop/security.nix
+++ modules/nixos/desktop/security.nix
@@ -1,5 +1,25 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   security.polkit.enable = true;
-  services.gnome.gnome-keyring.enable = true;
+  services.gnome = {
+    gnome-keyring.enable = true;
+    # aginst with ssh-agent
+    gcr-ssh-agent.enable = false;
+  };
+  # seahorse is a GUI App for GNOME Keyring.
+  programs.seahorse.enable = true;
+  # The OpenSSH agent remembers private keys for you
+  # so that you don’t have to type in passphrases every time you make an SSH connection.
+  # Use `ssh-add` to add a key to the agent.
+  programs.ssh.startAgent = true;
   security.pam.services.greetd.enableGnomeKeyring = true;
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryPackage = pkgs.pinentry-qt;
+    enableSSHSupport = false;
+    settings.default-cache-ttl = 4 * 60 * 60; # 4 hours
+  };
 }
