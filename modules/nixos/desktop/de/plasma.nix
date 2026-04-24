c5d2feacdab2a2c84a4cb1187bfa332a6e37ce17
mslxl <i@mslxl.com>
2026-04-24 19:59:39 +0800

    plasma: import plasma-manager via home-manager

--- modules/nixos/desktop/de/plasma.nix
+++ modules/nixos/desktop/de/plasma.nix
@@ -1,5 +1,6 @@
 # Error happens? back to plasma in temp
 {
+  plasma-manager,
   pkgs,
   config,
   lib,
@@ -7,29 +8,34 @@
   ...
 }:
 with lib; {
-  imports = [
-    inputs.plasma-manager.homeModules.plasma-manager
-  ];
-  config = mkIf (config.modules.desktop.type == "plasma") {
-    services.power-profiles-daemon.enable = true;
-    services.tlp.enable = mkForce false;
-    services.desktopManager.plasma6.enable = true;
-    environment.systemPackages = with pkgs; [
-      wl-clipboard
-      bottom
-    ];
-    home-manager.users.${myvars.username} = {
-      modules.desktop.type = "plasma";
-    };
+  config = mkMerge [
+    {
+      home-manager.users.${myvars.username}.imports = [
+        plasma-manager.homeModules.plasma-manager
+      ];
+    }
 
-    environment.plasma6.excludePackages = with pkgs.kdePackages; [
-      plasma-browser-integration
-      konsole
-      kmail
-      kate
-      kwallet
-      okular
-      elisa
-    ];
-  };
+    (mkIf (config.modules.desktop.type == "plasma") {
+      services.power-profiles-daemon.enable = true;
+      services.tlp.enable = mkForce false;
+      services.desktopManager.plasma6.enable = true;
+      environment.systemPackages = with pkgs; [
+        wl-clipboard
+        bottom
+      ];
+      home-manager.users.${myvars.username} = {
+        modules.desktop.type = "plasma";
+      };
+
+      environment.plasma6.excludePackages = with pkgs.kdePackages; [
+        plasma-browser-integration
+        konsole
+        kmail
+        kate
+        kwallet
+        okular
+        elisa
+      ];
+    })
+  ];
 }
