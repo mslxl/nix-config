52b62542ea2d6fd145d9751028769e28bd524722
mslxl <i@mslxl.com>
2026-08-10 11:13:05 +0800

    fix(nixos): migrate kmscon fonts to new config option

--- modules/nixos/desktop/fonts.nix
+++ modules/nixos/desktop/fonts.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{...}: {
   # all fonts are linked to /nix/var/nix/profiles/system/sw/share/X11/fonts
   fonts = {
     enableDefaultPackages = false;
@@ -43,19 +43,14 @@
     # It supports a richer feature set than the standard linux console VT,
     # including full unicode support, and when the video card supports drm should be much faster.
     enable = true;
-    fonts = with pkgs; [
-      {
-        name = "Maple Mono NF CN";
-        package = maple-mono.NF-CN-unhinted;
-      }
-      {
-        name = "JetBrainsMono Nerd Font";
-        package = nerd-fonts.jetbrains-mono;
-      }
-    ];
+    # Fonts are configured via `fonts.packages` (see ../base/fonts.nix);
+    # select the primary font for the console here.
+    config = {
+      "font-name" = "Maple Mono NF CN";
+      "font-size" = 16;
+      # Whether to use 3D hardware acceleration to render the console.
+      hwaccel = true;
+    };
     extraOptions = "--term xterm-256color";
-    extraConfig = "font-size=16";
-    # Whether to use 3D hardware acceleration to render the console.
-    hwRender = true;
   };
 }
